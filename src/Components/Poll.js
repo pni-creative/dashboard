806ff478d4ad10f281b4e97cab630537645ebc8b
Parallels <amir.ff@gmail.com>
2020-02-20 14:28:50 -0800

    get poll question from firebase

--- src/Components/Poll.js
+++ src/Components/Poll.js
@@ -7,9 +7,9 @@ class Poll extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-          question: "How's your day going?",
-          optionA: "😀",
-          optionB: "😫",
+          question: "",
+          optionA: "",
+          optionB: "",
           percentA: 0,
           percentB: 0,
           votesTotal: 0
@@ -20,6 +20,7 @@ class Poll extends React.Component {
     
         // Firebase configuration
         const firebaseConfig = {
+            
         };
   
         // Initialize Firebase
@@ -27,14 +28,32 @@ class Poll extends React.Component {
     
         const database = firebase.database();
         
-        database.ref().on('value', snapshot => {
+        database.ref('/admin/question').on('value', snapshot => {
+            this.setState({
+                question: snapshot.val()
+            });
+        });
+        
+        database.ref('/admin/optionA').on('value', snapshot => {
+            this.setState({
+                optionA: snapshot.val()
+            });
+        });
+        
+        database.ref('/admin/optionB').on('value', snapshot => {
+            this.setState({
+                optionB: snapshot.val()
+            });
+        });
+        
+        database.ref('/votes').on('value', snapshot => {
             
             let countA = 0;
             let countB = 0;
             
             snapshot.forEach((child) => { 
                 
-                if (child.val().vote === this.state.optionA) {
+                if (child.val().vote === "A") {
                     countA++;
                 } else {
                     countB++;
@@ -55,25 +74,25 @@ class Poll extends React.Component {
         return (
             <div className="box d poll">
                 <div className="title">
-                    <h1>Poll</h1>
+                    <p>Poll</p>
                 </div>
                 <h2>{this.state.question}</h2>
                 <ul>
                     <li>
-                        <span className="option">{this.state.optionA}</span>
+                        <span className="option">A. {this.state.optionA}</span>
                         <span className={`result ${this.state.percentA ? 'show' : null}`} style={aStyle}>
                             <span className="result-percent">{this.state.percentA}%</span>
                         </span>
                     </li>
                     <li>
-                        <span className="option">{this.state.optionB}</span>
+                        <span className="option">B. {this.state.optionB}</span>
                         <span className={`result ${this.state.percentB ? 'show' : null}`} style={bStyle}>
                             <span className="result-percent">{this.state.percentB}%</span>
                         </span>
                     </li>
                 </ul>
                 <p>Total votes: <strong>{this.state.votesTotal}</strong></p>
-                <p>Text your mood to <strong>604-373-3PNI (764)</strong></p>
+                <p>Text A or B to <strong>604-373-3PNI (764)</strong></p>
 
             </div>
         )
